6a16f97b65ff5466bca5af5897caf1dbe2c0ab32
faridaYeasmin <ashikcse4you@gmail.com>
2015-05-12 17:29:05 +0600

    cal

---
+++ StudentYearlyCostCalculatorApp/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace StudentYearlyCostCalculatorApp
+{
+    partial class YearlyCostCalculatorUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.classesComboBox = new System.Windows.Forms.ComboBox();
+            this.discountCheckBox = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.classesComboBox);
+            this.groupBox1.Controls.Add(this.discountCheckBox);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(402, 277);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Calculator for yearly cost of a student";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(120, 133);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(185, 41);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Calculate total amount for one year";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.totalAmountCalculateButton_Click);
+            // 
+            // classesComboBox
+            // 
+            this.classesComboBox.FormattingEnabled = true;
+            this.classesComboBox.Location = new System.Drawing.Point(120, 45);
+            this.classesComboBox.Name = "classesComboBox";
+            this.classesComboBox.Size = new System.Drawing.Size(185, 23);
+            this.classesComboBox.TabIndex = 4;
+            // 
+            // discountCheckBox
+            // 
+            this.discountCheckBox.AutoSize = true;
+            this.discountCheckBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.discountCheckBox.Location = new System.Drawing.Point(120, 84);
+            this.discountCheckBox.Name = "discountCheckBox";
+            this.discountCheckBox.Size = new System.Drawing.Size(221, 19);
+            this.discountCheckBox.TabIndex = 2;
+            this.discountCheckBox.Text = "Get 5% discount for early admission";
+            this.discountCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select Class : ";
+            // 
+            // YearlyCostCalculatorUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaGreen;
+            this.ClientSize = new System.Drawing.Size(426, 301);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "YearlyCostCalculatorUI";
+            this.Text = "Yearly Cost Calculator";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox discountCheckBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox classesComboBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ StudentYearlyCostCalculatorApp/Form1.cs
@@ -0,0 +1,344 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentYearlyCostCalculatorApp
+{
+    public partial class YearlyCostCalculatorUI : Form
+    {
+        public YearlyCostCalculatorUI()
+        {
+            InitializeComponent();
+
+            for (int i = 1; i <= 12; i++)
+            {
+                classesComboBox.Items.Add("Class "+i); 
+            }
+            
+        }
+
+        List<double> admissionFeeList = new List<double>()
+        {
+            10000,
+            10000,
+            10000,
+            10000,
+            10000,
+            12000,
+            12000,
+            12000,
+            15000,
+            15000,
+            15000,
+            15000,
+
+        };
+
+        List<double> monthlyFeeList = new List<double>()
+        {
+            500,
+            500,
+            1000,
+            1000,
+            1000,
+            1500,
+            1500,
+            1500,
+            2000,
+            2000,
+            2700,
+            2700,
+
+        };
+
+        //Dictionary<string,double> admissionFeeDictionary = new Dictionary<string, double>()
+        //{
+        //    {"Class 1",10000},
+        //    {"Class 2",10000},
+        //    {"Class 3",10000},
+        //    {"Class 4",10000},
+        //    {"Class 5",10000},
+        //    {"Class 6",12000},
+        //    {"Class 7",12000},
+        //    {"Class 8",12000},
+        //    {"Class 9",15000},
+        //    {"Class 10",15000},
+        //    {"Class 11",15000},
+        //    {"Class 12",15000},
+        //};
+        //Dictionary<string, double> monthlyFeeDictionary = new Dictionary<string, double>()
+        //{
+        //    {"Class 1",500},
+        //    {"Class 2",500},
+        //    {"Class 3",1000},
+        //    {"Class 4",1000},
+        //    {"Class 5",1000},
+        //    {"Class 6",1500},
+        //    {"Class 7",1500},
+        //    {"Class 8",1500},
+        //    {"Class 9",2000},
+        //    {"Class 10",2000},
+        //    {"Class 11",2700},
+        //    {"Class 12",2700},
+        //};
+
+
+
+        private void totalAmountCalculateButton_Click(object sender, EventArgs e)
+        {
+            if (classesComboBox.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select a class . ");
+            }
+            else
+            {
+                for (int i = 0; i < 12; i++)
+                {
+                    if (classesComboBox.SelectedIndex == i)
+                    {
+                        double totalAmount = admissionFeeList[i] + (monthlyFeeList[i] * 12);
+                        if (discountCheckBox.Checked == true)
+                        {
+
+                            double totalAmountWithDiscount = totalAmount - (totalAmount * 5) / 100;
+                            MessageBox.Show("Total amount for one year : " + Convert.ToString(totalAmountWithDiscount));
+                        }
+                        else
+                        {
+
+                            MessageBox.Show("Total amount for one year : " + Convert.ToString(totalAmount));
+                        }
+                    }
+                }
+
+
+                //if (classesComboBox.SelectedIndex == 0)
+                //{
+                //    double totalAmount = admissionFeeList[0] + (monthlyFeeList[0]*12);
+                //    if (discountCheckBox.Checked == true)
+                //    {
+
+                //        double totalAmountWithDiscount = totalAmount - (totalAmount*5)/100;
+                //        MessageBox.Show("Total amount for one year : " + Convert.ToString(totalAmountWithDiscount));
+                //    }
+                //    else
+                //    {
+
+                //        MessageBox.Show("Total amount for one year : " + Convert.ToString(totalAmount));
+                //    }
+
+
+                //}
+
+                //    if (classesComboBox.SelectedIndex == 1)
+                //    {
+                //        double totalAmount = admissionFeeDictionary["Class 2"] + (monthlyFeeDictionary["Class 2"] * 12);
+                //        if (discountCheckBox.Checked == true)
+                //        {
+
+                //            double totalAmountWithDiscount = totalAmount - (totalAmount * 5) / 100;
+                //            MessageBox.Show("Total amount for one year : " + Convert.ToString(totalAmountWithDiscount));
+                //        }
+                //        else
+                //        {
+
+                //            MessageBox.Show("Total amount for one year : " + Convert.ToString(totalAmount));
+                //        }
+
+
+                //    }
+
+                //    if (classesComboBox.SelectedIndex == 2)
+                //    {
+                //        double totalAmount = admissionFeeDictionary["Class 3"] + (monthlyFeeDictionary["Class 3"] * 12);
+                //        if (discountCheckBox.Checked == true)
+                //        {
+
+                //            double totalAmountWithDiscount = totalAmount - (totalAmount * 5) / 100;
+                //            MessageBox.Show("Total amount for one year : " + Convert.ToString(totalAmountWithDiscount));
+                //        }
+                //        else
+                //        {
+
+                //            MessageBox.Show("Total amount for one year : " + Convert.ToString(totalAmount));
+                //        }
+
+
+                //    }
+
+                //    if (classesComboBox.SelectedIndex == 3)
+                //    {
+                //        double totalAmount = admissionFeeDictionary["Class 4"] + (monthlyFeeDictionary["Class 4"] * 12);
+                //        if (discountCheckBox.Checked == true)
+                //        {
+
+                //            double totalAmountWithDiscount = totalAmount - (totalAmount * 5) / 100;
+                //            MessageBox.Show("Total amount for one year : " + Convert.ToString(totalAmountWithDiscount));
+                //        }
+                //        else
+                //        {
+
+                //            MessageBox.Show("Total amount for one year : " + Convert.ToString(totalAmount));
+                //        }
+
+
+                //    }
+
+                //    if (classesComboBox.SelectedIndex == 4)
+                //    {
+                //        double totalAmount = admissionFeeDictionary["Class 5"] + (monthlyFeeDictionary["Class 5"] * 12);
+                //        if (discountCheckBox.Checked == true)
+                //        {
+
+                //            double totalAmountWithDiscount = totalAmount - (totalAmount * 5) / 100;
+                //            MessageBox.Show("Total amount for one year : " + Convert.ToString(totalAmountWithDiscount));
+                //        }
+                //        else
+                //        {
+
+                //            MessageBox.Show("Total amount for one year : " + Convert.ToString(totalAmount));
+                //        }
+
+
+                //    }
+
+                //    if (classesComboBox.SelectedIndex == 5)
+                //    {
+                //        double totalAmount = admissionFeeDictionary["Class 6"] + (monthlyFeeDictionary["Class 6"] * 12);
+                //        if (discountCheckBox.Checked == true)
+                //        {
+
+                //            double totalAmountWithDiscount = totalAmount - (totalAmount * 5) / 100;
+                //            MessageBox.Show("Total amount for one year : " + Convert.ToString(totalAmountWithDiscount));
+                //        }
+                //        else
+                //        {
+
+                //            MessageBox.Show("Total amount for one year : " + Convert.ToString(totalAmount));
+                //        }
+
+
+                //    }
+
+                //    if (classesComboBox.SelectedIndex == 6)
+                //    {
+                //        double totalAmount = admissionFeeDictionary["Class 7"] + (monthlyFeeDictionary["Class 7"] * 12);
+                //        if (discountCheckBox.Checked == true)
+                //        {
+
+                //            double totalAmountWithDiscount = totalAmount - (totalAmount * 5) / 100;
+                //            MessageBox.Show("Total amount for one year : " + Convert.ToString(totalAmountWithDiscount));
+                //        }
+                //        else
+                //        {
+
+                //            MessageBox.Show("Total amount for one year : " + Convert.ToString(totalAmount));
+                //        }
+
+
+                //    }
+
+                //    if (classesComboBox.SelectedIndex == 7)
+                //    {
+                //        double totalAmount = admissionFeeDictionary["Class 8"] + (monthlyFeeDictionary["Class 8"] * 12);
+                //        if (discountCheckBox.Checked == true)
+                //        {
+
+                //            double totalAmountWithDiscount = totalAmount - (totalAmount * 5) / 100;
+                //            MessageBox.Show("Total amount for one year : " + Convert.ToString(totalAmountWithDiscount));
+                //        }
+                //        else
+                //        {
+
+                //            MessageBox.Show("Total amount for one year : " + Convert.ToString(totalAmount));
+                //        }
+
+
+                //    }
+
+                //    if (classesComboBox.SelectedIndex == 8)
+                //    {
+                //        double totalAmount = admissionFeeDictionary["Class 9"] + (monthlyFeeDictionary["Class 9"] * 12);
+                //        if (discountCheckBox.Checked == true)
+                //        {
+
+                //            double totalAmountWithDiscount = totalAmount - (totalAmount * 5) / 100;
+                //            MessageBox.Show("Total amount for one year : " + Convert.ToString(totalAmountWithDiscount));
+                //        }
+                //        else
+                //        {
+
+                //            MessageBox.Show("Total amount for one year : " + Convert.ToString(totalAmount));
+                //        }
+
+
+                //    }
+
+                //    if (classesComboBox.SelectedIndex == 9)
+                //    {
+                //        double totalAmount = admissionFeeDictionary["Class 10"] + (monthlyFeeDictionary["Class 10"] * 12);
+                //        if (discountCheckBox.Checked == true)
+                //        {
+
+                //            double totalAmountWithDiscount = totalAmount - (totalAmount * 5) / 100;
+                //            MessageBox.Show("Total amount for one year : " + Convert.ToString(totalAmountWithDiscount));
+                //        }
+                //        else
+                //        {
+
+                //            MessageBox.Show("Total amount for one year : " + Convert.ToString(totalAmount));
+                //        }
+
+
+                //    }
+
+                //    if (classesComboBox.SelectedIndex == 10)
+                //    {
+                //        double totalAmount = admissionFeeDictionary["Class 11"] + (monthlyFeeDictionary["Class 11"] * 12);
+                //        if (discountCheckBox.Checked == true)
+                //        {
+
+                //            double totalAmountWithDiscount = totalAmount - (totalAmount * 5) / 100;
+                //            MessageBox.Show("Total amount for one year : " + Convert.ToString(totalAmountWithDiscount));
+                //        }
+                //        else
+                //        {
+
+                //            MessageBox.Show("Total amount for one year : " + Convert.ToString(totalAmount));
+                //        }
+
+
+                //    }
+
+                //    if (classesComboBox.SelectedIndex == 11)
+                //    {
+                //        double totalAmount = admissionFeeDictionary["Class 12"] + (monthlyFeeDictionary["Class 12"] * 12);
+                //        if (discountCheckBox.Checked == true)
+                //        {
+
+                //            double totalAmountWithDiscount = totalAmount - (totalAmount * 5) / 100;
+                //            MessageBox.Show("Total amount for one year : " + Convert.ToString(totalAmountWithDiscount));
+                //        }
+                //        else
+                //        {
+
+                //            MessageBox.Show("Total amount for one year : " + Convert.ToString(totalAmount));
+                //        }
+
+
+                //    }
+                //}
+
+            }
+
+        }
+
+
+    }
+}
